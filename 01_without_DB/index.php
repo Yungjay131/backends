db7cbcbe28e1e01de023a079f1a479f577a1ab4d
Josh <talk2joshuasylvanus@gmail.com>
2021-10-13 18:24:27 +0100

    changed the image quality from medium to large in data.json

--- 01_without_DB/index.php
+++ 01_without_DB/index.php
@@ -1,6 +1,15 @@
 <?php
 require_once 'DataManager.php';
 
-$query_param = $_GET['users'] ?? 15;
+$query_param = $_GET['users'] ?? null;
 
-echo DataManager::getJSONData($query_param);
+$query_param_start = $_GET['from'] ?? 0;
+$query_param_end = $_GET['to'] ?? 15;
+
+if ($query_param) {
+    echo DataManager::getJSONData2($query_param);
+    exit;
+}
+
+echo DataManager::getJSONData($query_param_start, $query_param_end);
+exit;
