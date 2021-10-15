8f20940ac7e1257bc7cb3593326d0047bb444500
Josh <talk2joshuasylvanus@gmail.com>
2021-10-15 09:28:09 +0100

    put in safeguards to avoid inappropriate values in GET query and added hosts and httpd-vhosts files as examples of tweaking DNS

--- 01_without_DB/DataManager.php
+++ 01_without_DB/DataManager.php
@@ -2,24 +2,49 @@
 
 class DataManager {
     private static $filename = 'data.json';
-
+    private static $LIMIT = 299;
     public function __construct() {
     }
 
-    public static function getJSONData($query_param) {
+
+    public static function getJSONData($query_param_start, $query_param_end) {
+        $_data = self::getData();
+
+        $_result = [];
+
+        for ($index = $query_param_start; $index < $query_param_end; $index++) {
+            $_result[$index] = $_data[$index];
+        }
+
+        $__result = json_encode($_result);
+        return $__result;
+    }
+
+    public static function getJSONData2($number_of_users) {
+        $_data = self::getData();
+
+        $_result = [];
+
+        for ($index = 0; $index < $number_of_users; $index++) {
+            #making the data generated random
+            $_result[$index] = $_data[self::generateRandomNumber()];
+        }
+
+        $__result = json_encode($_result);
+        return $__result;
+    }
+
+    private static function getData() {
         $JSONData = file_get_contents(self::$filename);
 
         header('Content-type:application/json;charset-utf-8');
 
         $_data = json_decode($JSONData, true);
 
-        $_result = [];
+        return $_data;
+    }
 
-        for ($index = 0; $index < $query_param; $index++) {
-            $_result[$index] = $_data[$index];
-        }
-
-        $__result = json_encode($_result);
-        return $__result;
+    private static function generateRandomNumber() {
+        return rand(0, self::$LIMIT);
     }
 }
--- 01_without_DB/index.php
+++ 01_without_DB/index.php
@@ -2,9 +2,14 @@
 require_once 'DataManager.php';
 
 $query_param = $_GET['users'] ?? null;
+if ($query_param < 0 || $query_param > 299) $query_param = null;
+
 
 $query_param_start = $_GET['from'] ?? 0;
-$query_param_end = $_GET['to'] ?? 15;
+$query_param_start = ($query_param_start < 0 || $query_param_start > 299) ? 0 : $query_param_start;
+
+$query_param_end = $_GET['to'] ?? 50;
+if ($query_param_end < 1 || $query_param_end > 299) $query_param_end = 50;
 
 if ($query_param) {
     echo DataManager::getJSONData2($query_param);
